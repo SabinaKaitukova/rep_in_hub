61ef692a50428ba14e45e2fbdca6d6b095bf07a6
Sabina <sabinakait@mail.ru>
2024-12-24 18:29:59 +0300

    1. Изменен файл main теперь там находится разность чисел

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
-#include "sdt.h"
 #include <iostream>
+using namespace std;
 
 int main() {
+    int a, b;
+    cout << "Введите два числа: ";
+    cin >> a >> b;
+    cout << "Разность: " << a - b << endl;
     return 0;
 }
+
